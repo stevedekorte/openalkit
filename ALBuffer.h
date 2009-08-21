0555bf7bfbcd548852d08597bc6079c1858b9614
Steve Dekorte <steve@StevesLaptop-7.local>
2009-08-20 18:27:31 -0700

    latest

--- ALBuffer.h
+++ ALBuffer.h
@@ -8,20 +8,21 @@
 	ALuint alcBuffer;
 }
 
-+ bufferFromCacheForPath:(NSString *)path;
-
 @property (nonatomic, assign) NSData *data; 
 @property (nonatomic, assign) ALuint alcBuffer; 
 
++ bufferFromCacheForPath:(NSString *)path;
+
+// ugly - move these load methods to AudioData object later
+- (BOOL)loadURL:(NSURL *)url; 
+- (BOOL)loadPath:(NSString *)path;
+
 - (ALuint)alcBuffer;
 - (ALenum)alFormat;
+
 - (int)sampleRate;
 - (int)bitsPerSample;
 - (int)channels;
 - (int)size;
-
-// ugly - move to a sound file object
-- (BOOL)loadURL:(NSURL *)url; 
-- (BOOL)loadPath:(NSString *)path;
 
 @end
--- ALBuffer.m
+++ ALBuffer.m
@@ -218,5 +218,4 @@ Exit:
 	return i;
 }
 
-
 @end
